7da1aa776465fe897015796011fd5bd5946e5538
camilo escalona <camiloescalona1998@gmail.com>
2022-10-25 00:57:39 -0300

    avance 2 con profile

--- lib/Screens/profile_screen.dart
+++ lib/Screens/profile_screen.dart
@@ -1,5 +1,7 @@
+import 'package:accordion/controllers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:accordion/accordion.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -9,6 +11,15 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final _headerStyle = const TextStyle(
+      color: Color(0xffffffff), fontSize: 15, fontWeight: FontWeight.bold);
+  final _contentStyleHeader = const TextStyle(
+      color: Color(0xff999999), fontSize: 14, fontWeight: FontWeight.w700);
+  final _contentStyle = const TextStyle(
+      color: Color(0xff999999), fontSize: 14, fontWeight: FontWeight.normal);
+  final _loremIpsum =
+      '''Lorem ipsum is typically a corrupted version of 'De finibus bonorum et malorum', a 1st century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin.''';
+
   void viewModal(context, boxIndex) {
     switch (boxIndex) {
       case 1:
@@ -129,12 +140,14 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color.fromARGB(255, 24, 55, 120),
         appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 24, 55, 120),
+          elevation: 0,
           centerTitle: true,
-          backgroundColor: Colors.grey.shade300,
           title: const Text(
             'Deporte',
-            style: TextStyle(color: Colors.black),
+            style: TextStyle(color: Colors.white),
           ),
           actions: [
             IconButton(
@@ -142,82 +155,178 @@ class _ProfileState extends State<Profile> {
               icon: IconButton(
                 icon: const Icon(Icons.menu),
                 tooltip: 'Go to menu',
-                color: Colors.black,
+                color: Colors.white,
                 onPressed: () {},
               ),
             )
           ],
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 40),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 0),
-                        child: Column(
-                          children: [
-                            Text('123'),
-                            Text('Juegos'),
-                            Text('creados')
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Center(
-                        child: Container(
-                          width: 100,
-                          height: 100,
-                          decoration: BoxDecoration(
-                              color: Colors.red, shape: BoxShape.circle),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 0, right: 10),
-                        child: Column(
-                          children: [Text('123'), Text('Me gusta')],
-                        ),
-                      ),
-                    ),
-                  ]),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text('Juanito Perez'),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(1.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
                 children: [
-                  Icon(
-                    Icons.fmd_good_sharp,
-                    size: 20,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 10, right: 0),
+                              child: Column(
+                                children: [
+                                  Text('123', style: TextStyle(color: Colors.white)),
+                                  Text('Juegos', style: TextStyle(color: Colors.white)),
+                                  Text('creados', style: TextStyle(color: Colors.white))
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Center(
+                              child: Container(
+                                width: 100,
+                                height: 100,
+                                decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    shape: BoxShape.circle),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 0, right: 10),
+                              child: Column(
+                                children: [Text('123', style: TextStyle(color: Colors.white)), Text('Me gusta',style: TextStyle(color: Colors.white)  )],
+                              ),
+                            ),
+                          ),
+                        ]),
                   ),
-                  Text('Temuco, Chile'),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text('Juanito Perez', style: TextStyle(color: Colors.white)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(1.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.fmd_good_sharp,
+                          size: 20,
+                          color: Colors.white,
+                        ),
+                        Text('Temuco, Chile', style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, right: 20, top: 10),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 0.8,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Accordion(
+                    maxOpenSections: 2,
+                    headerBackgroundColorOpened: Colors.black54,
+                    headerBackgroundColor: Colors.black54,
+                    openAndCloseAnimation: true,
+                    headerPadding:
+                        const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
+                    sectionOpeningHapticFeedback: SectionHapticFeedback.heavy,
+                    sectionClosingHapticFeedback: SectionHapticFeedback.light,
+                    children: [
+                      AccordionSection(
+                        isOpen: true,
+                        leftIcon: const Icon(Icons.insights_rounded,
+                            color: Colors.white),
+                        headerBackgroundColor: Color.fromARGB(255, 24, 55, 120),
+                        headerBackgroundColorOpened: Colors.red,
+                        header: Text('Jugados', style: _headerStyle),
+                        content: Text(_loremIpsum, style: _contentStyle),
+                        contentHorizontalPadding: 20,
+                        contentBorderWidth: 1,
+                        // onOpenSection: () => print('onOpenSection ...'),
+                        // onCloseSection: () => print('onCloseSection ...'),
+                      ),
+                      AccordionSection(
+                        isOpen: false,
+                        leftIcon: const Icon(Icons.insights_rounded,
+                            color: Colors.white),
+                        headerBackgroundColor: Color.fromARGB(255, 24, 55, 120),
+                        headerBackgroundColorOpened: Colors.red,
+                        header: Text('Activos', style: _headerStyle),
+                        content: Text(_loremIpsum, style: _contentStyle),
+                        contentHorizontalPadding: 20,
+                        contentBorderWidth: 1,
+                        // onOpenSection: () => print('onOpenSection ...'),
+                        // onCloseSection: () => print('onCloseSection ...'),
+                      ),
+                      AccordionSection(
+                        isOpen: false,
+                        leftIcon: const Icon(Icons.insights_rounded,
+                            color: Colors.white),
+                        headerBackgroundColor: Color.fromARGB(255, 24, 55, 120),
+                        headerBackgroundColorOpened: Colors.red,
+                        header: Text('Ganados', style: _headerStyle),
+                        content: Text(_loremIpsum, style: _contentStyle),
+                        contentHorizontalPadding: 20,
+                        contentBorderWidth: 1,
+                        // onOpenSection: () => print('onOpenSection ...'),
+                        // onCloseSection: () => print('onCloseSection ...'),
+                      ),
+                      AccordionSection(
+                        isOpen: false,
+                        leftIcon: const Icon(Icons.insights_rounded,
+                            color: Colors.white),
+                        headerBackgroundColor: Color.fromARGB(255, 24, 55, 120),
+                        headerBackgroundColorOpened: Colors.red,
+                        header: Text('Comentarios', style: _headerStyle),
+                        content: Text(_loremIpsum, style: _contentStyle),
+                        contentHorizontalPadding: 20,
+                        contentBorderWidth: 1,
+                        // onOpenSection: () => print('onOpenSection ...'),
+                        // onCloseSection: () => print('onCloseSection ...'),
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: 0.8,
-                decoration: BoxDecoration(
-                  color: Colors.black,
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 80),
+                child: SizedBox(
+                  width: 300.0,
+                  height: 50.0,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 3,
+                      primary: Colors.lightBlue,
+                      onPrimary: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text('Agregar comentario',
+                        style: TextStyle(fontSize: 16)),
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ));
   }
 }
