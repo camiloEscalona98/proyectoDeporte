5ea792c782d96564efc70e45cd2baec57b5e7048
camilo escalona <camiloescalona1998@gmail.com>
2022-10-25 00:05:21 -0300

    avance con vista profile

--- lib/Screens/profile_screen.dart
+++ lib/Screens/profile_screen.dart
@@ -129,28 +129,95 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.grey.shade300,
-        title: const Text(
-          'Deporte',
-          style: TextStyle(color: Colors.black),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: IconButton(
-              icon: const Icon(Icons.menu),
-              tooltip: 'Go to menu',
-              color: Colors.black,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.grey.shade300,
+          title: const Text(
+            'Deporte',
+            style: TextStyle(color: Colors.black),
+          ),
+          actions: [
+            IconButton(
               onPressed: () {},
+              icon: IconButton(
+                icon: const Icon(Icons.menu),
+                tooltip: 'Go to menu',
+                color: Colors.black,
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10, right: 0),
+                        child: Column(
+                          children: [
+                            Text('123'),
+                            Text('Juegos'),
+                            Text('creados')
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Center(
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                              color: Colors.red, shape: BoxShape.circle),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 0, right: 10),
+                        child: Column(
+                          children: [Text('123'), Text('Me gusta')],
+                        ),
+                      ),
+                    ),
+                  ]),
             ),
-          )
-        ],
-      ),
-      body: Column(
-        children: [],
-      ),
-    );
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text('Juanito Perez'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.fmd_good_sharp,
+                    size: 20,
+                  ),
+                  Text('Temuco, Chile'),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
